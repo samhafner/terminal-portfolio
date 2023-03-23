a3d87755e7ebaf653494f3aaf6823d7d590199b3
Samuel Hafner <samuel.hafner@pm.me>
2023-03-23 21:58:34 +0100

    focus input after input component is shwon again

--- src/components/TerminalInput.vue
+++ src/components/TerminalInput.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useTerminalStore } from '../stores/terminal.store';
 
 //#region PROPS AND EMITS
 const emit = defineEmits<{
@@ -35,9 +36,25 @@ useEventListener(document, 'keydown', (e) => {
 })
 //#endregion
 // ====================================================================================================
-//#region FOCUS INPUT ON CLICK
+//#region FOCUS INPUT
+// Focus on every click on the page if there is not a component processing
+const terminalStore = useTerminalStore()
 useEventListener(document, 'click', (e) => {
-    terminalInput.value?.focus();
+    if (!terminalStore.componentIsProcessing) {
+        terminalInput.value?.focus();
+    }
+})
+
+// Focus on the input when this input is shown again after a component has finished processing
+watch(() => terminalStore.componentIsProcessing, () => {
+    if (!terminalStore.componentIsProcessing) {
+        // Wait for 500ms before focusing on the input so that the input line is scrolled into view
+        // A timeout (of at least 0ms)  is necessary otherwise the autofocus will not work
+        // 500ms works for now
+        setTimeout(() => {
+            terminalInput.value?.focus();
+        }, 500)
+    }
 })
 //#endregion
 
@@ -48,8 +65,9 @@ useEventListener(document, 'click', (e) => {
         <form @submit.prevent="emit('command-enter')">
             <div class="flex gap-2">
                 <span class="sm:hidden">> </span>
-                <input v-model="command" id="terminal-input" ref="terminalInput" type="text" class="w-full outline-none bg-inherit text-inherit caret-indigo-600 "
-                    @keyup="keyHandler" autofocus autocomplete="off" autocapitalize="off" spellcheck="false">
+                <input v-model="command" id="terminal-input" ref="terminalInput" type="text"
+                    class="w-full outline-none bg-inherit text-inherit caret-indigo-600 " @keyup="keyHandler" autofocus
+                    autocomplete="off" autocapitalize="off" spellcheck="false">
             </div>
             <label for="terminal-input" class="sr-only">Terminal Command Input</label>
         </form>
